4a14c1c8d9745389dada532f55d7a2d05bd68dc9
00011585 <wiut00011585@gmail.com>
2021-04-07 13:45:04 -0400

    create page with json

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.set('view engine', 'pug')
 
@@ -12,5 +13,45 @@ app.use('/static', express.static('./public'))
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+
+app.get('/blog/create', (req, res) => {
+  res.render('create')
+})
+
+app.post('/blog/create', (req, res) => {
+  const blog = {
+    topic: req.body.topic,
+    title: req.body.title,
+    body: req.body.body,
+    isNew: true,
+    id: '_' + Math.random().toString(36).substr(2,9)
+  }
+
+
+  if (blog.title.trim() === '' || blog.topic.trim() === '' || blog.body.trim() == ''){
+    res.render('/')
+  } else {
+    fs.readFile('./data/blogs.json', (err, data) => {
+      if (err) throw err
+      const blogs = JSON.parse(data)
+      blogs.push(blog)
+      fs.writeFile('./data/blogs.json', JSON.stringify(blogs), (err) => {
+        if (err) throw err
+
+        res.render('index')
+      })
+    })
+  }
+})
+
+app.get('api/v1/blogs', (req, res) => {
+  fs.readFile('./data/blogs.json', (err, data) => {
+    if (err) throw err
+    const blogs = JSON.parse(data)
+    res.json(blogs)
+  })
+})
+
 
 app.listen(5050, () => console.log('App is running on port 5050...'))

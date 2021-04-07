fd07a07a88193553898958d30ccd8f1e61945b83
00011585 <wiut00011585@gmail.com>
2021-04-07 12:48:37 -0400

    index page with styling added

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const express = require('express')
 const app = express()
+
+app.set('view engine', 'pug')
+
+// app.use('/blogs', blogs)
+
+app.use(express.urlencoded( {extended: false }))
+
+app.use('/static', express.static('./public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(5050, () => console.log('App is running on port 5050...'))
